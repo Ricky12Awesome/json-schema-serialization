91298bfe240a4e3362c500c49107a4e4e8907ddd
Ricky12Awesome <rickygraveen@gmail.com>
2020-07-07 16:10:05 -0500

    Added Bintray repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+import com.jfrog.bintray.gradle.BintrayExtension.PackageConfig
+import com.jfrog.bintray.gradle.BintrayExtension.VersionConfig
+import org.jetbrains.kotlin.konan.properties.Properties
+
 plugins {
   kotlin("multiplatform") version "1.3.72"
   kotlin("plugin.serialization") version "1.3.72"
+  id("com.jfrog.bintray") version "1.8.5"
   `maven-publish`
 }
 
@@ -66,3 +71,42 @@ kotlin {
     }
   }
 }
+
+val properties = Properties().apply {
+  val file = file("local.properties")
+  if (file.exists()) {
+    file.inputStream().use {
+      load(it)
+    }
+  }
+}
+
+val bintrayApiKey: String? = properties.getProperty("BINTRAY_API_KEY")
+val bintrayUser: String? = properties.getProperty("BINTRAY_USER")
+val bintrayRepo: String? = properties.getProperty("BINTRAY_REPO")
+
+if (bintrayApiKey != null && bintrayUser != null || bintrayRepo != null) {
+  bintray {
+    key = bintrayApiKey
+    user = bintrayUser
+    publish = false
+
+    setPublications(*publishing.publications
+      .map { it.name }
+      .filter { it != "kotlinMultiplatform" }
+      .toTypedArray()
+    )
+
+    configure(pkg, closureOf<PackageConfig> {
+      repo = bintrayRepo
+      name = project.name
+      vcsUrl = "https://github.com/Ricky12Awesome/json-schema-serialization.git"
+      setLicenses("MIT")
+
+      configure(version, closureOf<VersionConfig> {
+        name = project.version.toString()
+      })
+    })
+  }
+}
+
